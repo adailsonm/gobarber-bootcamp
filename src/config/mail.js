f72478179fd78a7e0a09b90e95f05aca6d606174
Adailson Moreira <adailsonmoreira@MacBook-Pro-de-Adailson.local>
2019-08-29 23:20:25 -0300

    Dando continuidade

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '630f68eef5d1c2',
+    pass: 'c5feedfe652237',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
